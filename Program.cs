9b385a5a2fdc19f1d0881cb23d1531628cb77cd2
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-31 12:12:48 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisonNonStatic
+{
+    public class Method
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Welcome to the Line Comparison computation Program for Non Static Methods.*****");
+
+        }
+    }
+}
